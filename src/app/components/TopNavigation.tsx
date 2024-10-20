9bd9ea73a8a77fe78b7ab49047f3a173eb810d9a
Bhargava1424 <sribhargava2002@gmail.com>
2024-10-20 19:27:19 -0400

    toggle theme

--- src/app/components/TopNavigation.tsx
+++ src/app/components/TopNavigation.tsx
@@ -60,9 +60,10 @@ export const TopNavigation: React.FC<{
           <Button
             variant="outline"
             size="icon"
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           >
-            {theme === 'dark' ? <Sun className="h-4 w-4" /> : <Moon className="h-4 w-4" />}
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
           </Button>
           <Button
